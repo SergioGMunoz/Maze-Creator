fa58d78307de47bfa2807aa91aecdd8ef14a78a0
SergioGMunoz <sergiogmunozdev@gmail.com>
2025-05-16 12:57:30 +0200

    Changes in connection and launcher, added clases for game funcionality

--- src/model/ConnectionDB.java
+++ src/model/ConnectionDB.java
@@ -4,60 +4,47 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
 public class ConnectionDB {
-    String user;
-    String pwd;
-    String host;
-    String port;
-    String dbName;
-    String url;
-    Connection connection;
 
+    private static Connection connection = null;
 
-    public ConnectionDB(String user, String pwd, String host, String port, String dbName) {
-        this.user = user;
-        this.pwd = pwd;
-        this.host = host;
-        this.port = port;
-        this.dbName = dbName;
-        this.url = "jdbc:mysql://" + host + ":" + port + "/" + dbName;
-        connect();
+    public static Connection connect(String user, String pwd, String host, String port, String dbName) {
+        String url = "jdbc:mysql://" + host + ":" + port + "/" + dbName;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            setConnection(DriverManager.getConnection(url, user, pwd));
+            System.out.println("✔️ Conexión exitosa con la BBDD: " + dbName);
+        } catch (ClassNotFoundException e) {
+            System.err.println("❌ Driver JDBC no encontrado");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.err.println("❌ Error al conectarse a la BBDD");
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.err.println("❌ Error insesperado de conexión con la BBDD");
+            e.printStackTrace();
+        }
+        return getConnection();
     }
-    
-    public Connection getConnection() {
+
+    public static void close() {
+        try {
+            if (getConnection() != null && !getConnection().isClosed()) {
+                getConnection().close();
+                System.out.println("✔️ Conexión cerrada correctamente");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.err.println("❌ Error inesperado");
+        }
+    }
+
+	public static Connection getConnection() {
 		return connection;
 	}
 
-	public void connect() {
-    	Connection connection = null;
-    	try {
-			Class.forName("com.mysql.cj.jdbc.Driver"); 
-			connection = DriverManager.getConnection(url, user, pwd);
-			System.out.println("✔️ Conexión exitosa con la BBDD: " + dbName);
-    	} catch (ClassNotFoundException e) {
-			System.err.println("❌ Driver JDBC No encontrado");
-			e.printStackTrace();
-		} catch (SQLException e) {
-			System.err.println("❌ Error al conectarse a la BD");
-			e.printStackTrace();
-		} catch (Exception e) {
-			System.err.println("❌ Error general de Conexión");
-			e.printStackTrace();
-		}
-    }
-    
-    public boolean close() {
-    	try {
-    		connection.close();
-    		System.out.println("✔️ Conexión cerrada correctamente");
-    		return true;
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}catch(Exception e) {
-			System.err.println("❌ Error inesperado");
-		}
-    	return false;
-    }
+	public static void setConnection(Connection connection) {
+		ConnectionDB.connection = connection;
+	}
 }
-
