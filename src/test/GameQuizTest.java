aba3beb369e5edc7a41c211642824a67e47b7636
Sergio G. Muñoz <sergiogmunozdev@gmail.com>
2025-05-17 00:25:08 +0200

    ⚡Test Game and Quiz

--- src/test/GameQuizTest.java
+++ src/test/GameQuizTest.java
@@ -12,7 +12,7 @@ public class GameQuizTest {
         if (ConnectionDB.connect("JavaLaberinto", "Java12345", "localhost", "3306", "Laberinto") != null) {
             //Crear partida de prueba ID  1 dispo 1
         	try {
-				GameController gc = new GameController(2,1);
+				GameController gc = new GameController(1,1);
 			} catch (SQLException e) {
 				System.out.println("Error de SQL al crear GameController");
 				e.printStackTrace();
